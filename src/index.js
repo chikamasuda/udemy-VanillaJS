f7453cd4787514aa2deadd87d13a6bca4eab1d02
chikamasuda <66733811+chikamasuda@users.noreply.github.com>
2022-01-14 02:04:13 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+/**
+ * const, letなどの変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+//const val3 = "const変数";
+//console.log(val3);
+
+//const変数は文字列、数値は上書き、再宣言不可
+// val3 = "const変数を上書き";
+//完了リストに追加する要素
+// const val4 = {
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+
+//constで定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
