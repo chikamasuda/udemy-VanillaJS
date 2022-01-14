03bd85a72083527eae30cc8871238085a3b3f9c1
chikamasuda <66733811+chikamasuda@users.noreply.github.com>
2022-01-15 04:11:06 +0900

    22.デフォルト値まで終了

--- src/index.js
+++ src/index.js
@@ -39,3 +39,72 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "じゃけえ";
+// const age = 28;
+
+//私の名前はじゃけえです。年齢は28歳です。
+//従来の方法
+// const message1 = "私の名前は" + name + "です。名前は" + age + "です。";
+// console.log(message1);
+
+//テンプレート文字列を用いた方法
+// const mes sage2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+//アロー関数
+//処理が一行で終わる時はreturnや{}を省略できる
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => num1 + num2;
+
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+//特定のプロパティを抜き出せる
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// const myProfile = ["じゃけえ", 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+
+// console.log(message3);
+
+//配列の順番通りに値が代入される。順番に気を付ける。名前は決まっていない。nameにじゃけえが入る。
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など。引数の変数の後に入っている値がデフォルト値
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello("じゃけえ");
