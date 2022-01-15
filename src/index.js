654de5eac50b7286045917ccc4f69777c065e17d
chikamasuda <66733811+chikamasuda@users.noreply.github.com>
2022-01-16 04:46:00 +0900

    文法セクション終わり

--- src/index.js
+++ src/index.js
@@ -106,5 +106,129 @@
 /**
  * デフォルト値、引数など。引数の変数の後に入っている値がデフォルト値
  */
-const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
-sayHello("じゃけえ");
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello("じゃけえ");
+
+/**
+ * スプレット構文 ...
+ */
+//配列の展開
+
+// const arr1 = [1, 2];
+// console.log(arr1); //[1,2]
+
+//配列の中身を順番に展開する。
+// console.log(...arr1); //1,2
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+//配列の中身をまとめて受け取る
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+//コピー
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+//結合
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+//下記だと参照も引き継いだままコピーしてしまうため、片方を変更したらもう片方の配列も影響を受けてしまう。
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+//const nameArr = ["田中", "山田", "じゃけぇ"];
+
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+//中に順番に要素が設定される。
+//単純に配列を設定するのとリターンされた値に基づいて再度配列を設定するものがある。
+//nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+//const num = 1300;
+//console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています１！" : "許容範囲内です";
+// };
+
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// ||　は左側がfalseなら右側を返す、左側がtrueなら左側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+
+// console.log(fee);
+
+//&&は左側がtrueなら右側を返す, 左側がfalseだと左側が返却される。
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
